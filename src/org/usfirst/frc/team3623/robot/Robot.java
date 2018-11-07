3348e4c79c30ff76f096ca0fc737fe12cce3153f
Eric Jenny <31317958+ericjenny3623@users.noreply.github.com>
2018-11-07 15:46:03 -0500

    Initial commit

---
+++ src/org/usfirst/frc/team3623/robot/Robot.java
@@ -0,0 +1,117 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc.team3623.robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the build.properties file in the
+ * project.
+ */
+public class Robot extends IterativeRobot {
+	private static final String kDefaultAuto = "Default";
+	private static final String kCustomAuto = "My Auto";
+	private String m_autoSelected;
+	private SendableChooser<String> m_chooser = new SendableChooser<>();
+	Spark m_frontLeft = new Spark(1);	
+	Spark m_rearLeft1 = new Spark(2);
+	Spark m_frontRight = new Spark(3);	
+	Spark m_rearRight = new Spark(4);
+	//Define joystick being used at USB port 1 on the Driver Station
+	Joystick m_driveStick = new Joystick(1);
+	MecanumDrive m_robotDrive = new MecanumDrive(m_frontLeft,  m_rearLeft1, m_frontRight,m_rearRight);
+ 
+
+	/* 
+	 * Simplest program to drive a robot with mecanum drive using a single Logitech
+	 * Extreme 3D Pro joystick and 4 drive motors connected as follows:
+	 *     - PWM 0 - Connected to front left drive motor
+	 *     - PWM 1 - Connected to rear left drive motor
+	 *     - PWM 2 - Connected to front right drive motor
+	 *     - PWM 3 - Connected to rear right drive motor
+	 */
+
+	//Create a robot drive object using PWMs 0, 1, 2 and 3
+		
+	
+	public void teleopPeriodic1() 
+	{
+		m_robotDrive.driveCartesian(m_driveStick.getRawAxis(1),m_driveStick.getRawAxis(0),m_driveStick.getRawAxis(4));
+	}
+
+
+	/**
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
+	 */
+	@Override
+	public void robotInit() {
+		m_chooser.addDefault("Default Auto", kDefaultAuto);
+		m_chooser.addObject("My Auto", kCustomAuto);
+		SmartDashboard.putData("Auto choices", m_chooser);
+	}
+
+	/**
+	 * This autonomous (along with the chooser code above) shows how to select
+	 * between different autonomous modes using the dashboard. The sendable
+	 * chooser code works with the Java SmartDashboard. If you prefer the
+	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+	 * getString line to get the auto name from the text box below the Gyro
+	 *
+	 * <p>You can add additional auto modes by adding additional comparisons to
+	 * the switch structure below with additional strings. If using the
+	 * SendableChooser make sure to add them to the chooser code above as well.
+	 */
+	@Override
+	public void autonomousInit() {
+		m_autoSelected = m_chooser.getSelected();
+		// autoSelected = SmartDashboard.getString("Auto Selector",
+		// defaultAuto);
+		System.out.println("Auto selected: " + m_autoSelected);
+	}
+
+	/**
+	 * This function is called periodically during autonomous.
+	 */
+	@Override
+	public void autonomousPeriodic() {
+		switch (m_autoSelected) {
+		case kCustomAuto:
+			// Put custom auto code here
+			break;
+		case kDefaultAuto:
+		default:
+			// Put default auto code here
+			break;
+		}
+	}
+
+	/**
+	 * This function is called periodically during operator control.
+	 */
+	@Override
+	public void teleopPeriodic() {
+	}
+
+	/**
+	 * This function is called periodically during test mode.
+	 */
+	@Override
+	public void testPeriodic() {
+	}
+}
